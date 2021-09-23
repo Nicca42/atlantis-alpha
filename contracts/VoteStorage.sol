f7e1ba754615703dd48aa2ba9f0ef504094108bc
Veronica <veronica.21.coutts@gmail.com>
2021-09-24 00:17:54 +0200

    Vote Storage

---
+++ contracts/VoteStorage.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract VoteStorage {
+    // TODO make registry for vote types & consensus types. Need to be careful
+    //      about design to make sure it is highly extendable but protects 
+    //      against votes getting lost with upgrades. 
+
+    // function addVoteType(bytes32 _typeID) external 
+
+}
