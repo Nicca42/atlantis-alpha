d2f4e20b2ef4a1b1a5cb53c2525af81d56b4934d
Veronica <veronica.21.coutts@gmail.com>
2021-09-28 18:09:46 +0200

    Core progress on exe

--- contracts/Core.sol
+++ contracts/Core.sol
@@ -11,6 +11,21 @@ interface IExe {
     );
 }
 
+interface ICoord {
+    /**
+     * @param   _propID ID of the proposal to execute. 
+     * @notice  This function will check that the specified proposal has reached
+     *          quorum, and that it has passed. If the proposal has not reached
+     *          quorum or has not passed this will return false. 
+     * @dev     This function will update the state for the proposal if it has
+     *          not already been marked as executable. 
+     */
+    function execute(uint256 _propID) external returns(bool);
+
+    // QS update to use just this. 
+    function isExecutable(bytes32 _exeID) external view returns(bool);
+}
+
 contract Core {
     bytes32 public constant IDENTIFIER = bytes32(keccak256("CORE"));
 
@@ -21,14 +36,31 @@ contract Core {
 
     mapping(bytes32 => Instance) private ecosystem_;
 
-    // QS constructor calls loop to add all core contracts at deploy
-
     modifier onlyCore() {
         require(msg.sender == address(this), "Core: Only exes can modify");
         _;
     }
 
-    constructor(address[] memory _instances) {}
+    constructor(
+        address _coord,
+        address _exes,
+        address _props,
+        address _voteWeight,
+        address _votingBooth,
+        address _voteStorage
+    ) {
+        _addContract(CoreLib.COORD, _coord);
+        _addContract(CoreLib.EXE, _exes);
+        _addContract(CoreLib.PROPS, _props);
+        _addContract(CoreLib.VOTE_WEIGHT, _voteWeight);
+        _addContract(CoreLib.VOTE_BOOTH, _votingBooth);
+        _addContract(CoreLib.VOTE_STORAGE, _voteStorage);
+    }
+
+
+    //--------------------------------------------------------------------------
+    // VIEW & PURE FUNCTIONS
+    //--------------------------------------------------------------------------
 
     function getInstance(bytes32 _key) external view returns(address) {
         return ecosystem_[_key].implementation;
@@ -41,51 +73,27 @@ contract Core {
         );
     }
 
-    function addContract(
-        bytes32 _key,
-        address _instance,
-        bytes4 _function
-    ) external onlyCore {
-        require(
-            ecosystem_[_key].implementation == address(0),
-            "Core: use update to replace"
-        );
-        _addContract(_key, _instance, _function);
-    }
-
-    function updateContract(
-        bytes32 _key,
-        address _instance,
-        bytes4 _function
-    ) external onlyCore {
-        _addContract(_key, _instance, _function);
-    }
+    //--------------------------------------------------------------------------
+    // PUBLIC & EXTERNAL FUNCTIONS
+    //--------------------------------------------------------------------------
 
     function execute(bytes32 _exeID) external {
-        // TODO check proposal is valid for execution
-        // NOTE what if we don't verify the prop passes here, but do that in the
-            // coordinator. Then here all we have to do is execute. 
-            // Could even call the coord to make sure that the Exe has exectuion rights,
-            // then it can verify that the prop it is connected to has passed. 
+        ICoord coord = ICoord(this.getInstance(CoreLib.COORD));
 
-        // (address coord, bytes4 sig) = this.getContract(CoreLib.COORD);
-        // (
-        //     bool success,
-        //     bytes memory returnData
-        // ) = coord.call{value:0}(
-        //     abi.encodePacked(
-        //         sig, 
-        //         _propID
-        //     )
-        // );
+        require(
+            coord.isExecutable(_exeID),
+            "Core: Exe is not executable"
+        );
 
-        (address exe, ) = this.getContract(CoreLib.EXE);
+        IExe exe = IExe(this.getInstance(CoreLib.EXE));
+
+        // Gets the data for the executable
         (
             address[] memory targets,
             bytes[] memory callData,
             uint256[] memory values
-        ) = IExe(exe).getExe(_exeID);
-
+        ) = exe.getExe(_exeID);
+        // Executes each step of the executable
         for (uint256 i = 0; i < targets.length; i++) {
             (bool success, ) = targets[i].call{
                     value: values[i]
@@ -96,12 +104,60 @@ contract Core {
         }
     }
 
-    function _addContract(
+    //--------------------------------------------------------------------------
+    // ONLY CORE 
+    // 
+    // Below functions can only be called by this contract. These functions can
+    // only be executed by successfully running an executable through the 
+    // `execute` function. 
+
+    function addContract(
         bytes32 _key,
         address _instance,
         bytes4 _function
+    ) external onlyCore {
+        require(
+            ecosystem_[_key].implementation == address(0),
+            "Core: Use update to replace"
+        );
+        require(_instance != address(0), "Core: Cannot delete on add");
+
+        _addContract(_key, _instance);
+    }
+
+    function updateContract(
+        bytes32 _key,
+        address _instance,
+        bytes4 _function
+    ) external onlyCore {
+        require(
+            ecosystem_[_key].implementation != address(0),
+            "Core: Cannot add on update"
+        );
+        require(_instance != address(0), "Core: Cannot delete on update");
+
+        _addContract(_key, _instance);
+    }
+
+    function deleteContract(
+        bytes32 _key
+    ) external onlyCore {
+        require(
+            ecosystem_[_key].implementation != address(0),
+            "Core: already deleted"
+        );
+
+        _addContract(_key, address(0));
+    }
+
+    //--------------------------------------------------------------------------
+    // PRIVATE & INTERNAL FUNCTIONS
+    //--------------------------------------------------------------------------
+
+    function _addContract(
+        bytes32 _key,
+        address _instance
     ) internal {
         ecosystem_[_key].implementation = _instance;
-        ecosystem_[_key].functionSig = _function;
     }
 }
--- contracts/CoreLib.sol
+++ contracts/CoreLib.sol
@@ -2,10 +2,11 @@
 pragma solidity 0.8.7;
 
 library CoreLib {
+    bytes32 constant public CORE = keccak256("CORE");
     bytes32 constant public COORD = keccak256("COORDINATOR");
     bytes32 constant public EXE = keccak256("EXECUTABLES");
     bytes32 constant public PROPS = keccak256("PROPOSALS");
     bytes32 constant public VOTE_WEIGHT = keccak256("VOTE_WEIGHT");
     bytes32 constant public VOTE_BOOTH = keccak256("VOTING_BOOTH");
-    bytes32 constant public VOTE_STORAGE = keccak256("VOTING_STORAGE");
+    bytes32 constant public VOTE_STORAGE = keccak256("VOTE_STORAGE");
 }
